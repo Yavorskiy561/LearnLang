a071f73d77dcd1808753ee2102a064cff8c78967
Yavorskiy561 <115639880+Yavorskiy561@users.noreply.github.com>
2024-04-05 22:03:07 +0700

    Add files via upload

--- LearnLang/js/script.js
+++ LearnLang/js/script.js
@@ -1,15 +1,5 @@
-const element = document.querySelector('div,img,p');
-
-// Create an IntersectionObserver instance
-const observer = new IntersectionObserver((entries) => {
-  // Check if the element is intersecting with the viewport
-  const isIntersecting = entries[0].isIntersecting;
-
-  // If the element is intersecting, play the animation
-  if (isIntersecting) {
-    element.style.animation = 'textAnimation 3s';
-  }
-}, { threshold: 1 });
-
-// Observe the element
-observer.observe(element);
+const image = document.querySelector('img');
+function FlipVertical()
+{
+  image.classList.toggle('flip-vertical')
+}
